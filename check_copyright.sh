de77b7c41bf5aecffab6b9795cebf4ade988e506
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-12-03 17:54:34 +0100

    Fix CI

--- check_copyright.sh
+++ check_copyright.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 EXIT=0
-for file in $(find src -name "*.rs"); do
+for file in $(find examples lib -name "*.rs"); do
     if ! grep -E -q "Copyright \(c\) 20[0-9]{2}(-20[0-9]{2})? Bastiaan van der Plaat" "$file"; then
         echo "Bad copyright header in: $file"
         EXIT=1
