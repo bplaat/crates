bca231861fc9c923959f24ceb33c6e9452799507
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-12-08 11:42:59 +0100

    validate: Use better email check regex

--- lib/validate/src/lib.rs
+++ lib/validate/src/lib.rs
@@ -61,6 +61,8 @@ pub use validate_derive::Validate;
 
 #[cfg(feature = "email")]
 pub fn is_valid_email(email: &str) -> bool {
-    let re = regex::Regex::new(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").unwrap();
+    let re =
+        regex::Regex::new(r"^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")
+            .unwrap();
     re.is_match(email)
 }
