8ecf2502d2ecbbb454f67de94de98b6ee99dbc18
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-01-02 23:58:22 +0100

    webview: Add simple macOS webview library

---
+++ examples/webview-ipc/src/main.rs
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2025 Bastiaan van der Plaat
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+//! A webview ipc example
+
+use serde::{Deserialize, Serialize};
+use webview::{Event, WebviewBuilder};
+
+const APP_HTML: &[u8] = include_bytes!("../app.html");
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum IpcMessage {
+    #[serde(rename = "hello")]
+    Hello { name: String },
+}
+
+fn main() {
+    let mut webview = WebviewBuilder::new()
+        .title("Webview IPC Example")
+        .size(1024, 768)
+        .min_size(640, 480)
+        .remember_window_state(true)
+        .enable_ipc(true)
+        .html(std::str::from_utf8(APP_HTML).expect("Not valid UTF-8"))
+        .build();
+
+    webview.run(|webview, event| match event {
+        Event::PageLoaded => {
+            let message = IpcMessage::Hello {
+                name: "WebView".to_string(),
+            };
+            webview.eval(format!(
+                "window.ipc.dispatchEvent(new MessageEvent('message', {{ data: {} }}));",
+                serde_json::to_string(&message).expect("Should serialize message")
+            ));
+        }
+        Event::IpcMessageReceived(message) => {
+            match serde_json::from_str(&message).expect("Can't parse message") {
+                IpcMessage::Hello { name } => {
+                    println!("Hello, {}!", name);
+                }
+            }
+        }
+    });
+}
