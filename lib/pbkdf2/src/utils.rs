9ef1f31652abacc81a209fe2b37f0061df2bbb31
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-01-06 17:30:23 +0100

    pbkdf2: Clean up code a bit

--- lib/pbkdf2/src/utils.rs
+++ lib/pbkdf2/src/utils.rs
@@ -11,13 +11,13 @@ use crate::pbkdf2_hmac_sha256;
 
 /// Hash password using PBKDF2-HMAC-SHA256 returns string in PHC standard (https://github.com/P-H-C/phc-string-format/blob/master/phc-sf-spec.md)
 pub fn password_hash(password: &str) -> String {
+    const ITERATIONS: u32 = 100_000;
     let mut salt = [0u8; 16];
     getrandom::getrandom(&mut salt).expect("Can't get random bytes");
-    let iterations = 100_000;
-    let hashed_password = pbkdf2_hmac_sha256(password.as_bytes(), &salt, iterations, 32);
+    let hashed_password = pbkdf2_hmac_sha256(password.as_bytes(), &salt, ITERATIONS, 32);
     format!(
         "$pbkdf2-sha256$t={}${}${}",
-        iterations,
+        ITERATIONS,
         base64::encode(&salt, false),
         base64::encode(&hashed_password, false)
     )
@@ -35,7 +35,7 @@ pub fn password_verify(password: &str, hash: &str) -> Result<bool, PasswordHashD
     let salt = base64::decode(parts[3], false).map_err(|_| PasswordHashDecodeError)?;
     let stored_hash = base64::decode(parts[4], false).map_err(|_| PasswordHashDecodeError)?;
     let computed_hash = pbkdf2_hmac_sha256(password.as_bytes(), &salt, iterations, 32);
-    Ok(computed_hash == stored_hash)
+    Ok(stored_hash == computed_hash)
 }
 
 // MARK: PasswordHashDecodeError
