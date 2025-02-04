fb1da914c37ff6443476ba1ec7fdb5fe7f2316a2
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-02-04 07:52:08 +0100

    useragent: Add basic example

---
+++ lib/useragent/examples/useragent-parser.rs
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 Bastiaan van der Plaat
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+//! An example of the user agent parser
+
+use useragent::UserAgentParser;
+
+fn main() {
+    let parser = UserAgentParser::new();
+    let ua = parser.parse(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:134.0) Gecko/20100101 Firefox/134.0",
+    );
+    assert_eq!(ua.client.family, "Firefox");
+    assert_eq!(ua.client.version, Some("134.0".to_string()));
+    assert_eq!(ua.os.family, "Mac OS X");
+    assert_eq!(ua.os.version, Some("10.15".to_string()));
+}
