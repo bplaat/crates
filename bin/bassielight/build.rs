5c679089e5f2ef33d2a7e5d061fc5e2bfb22bc34
lplaat <leonard.van.der.plaat@gmail.com>
2025-06-17 15:37:08 +0200

    bassielight: Added preact and made it look better

--- bin/bassielight/build.rs
+++ bin/bassielight/build.rs
@@ -7,10 +7,62 @@
 #![doc = include_str!("README.md")]
 
 fn main() {
-    // Minify app.html
-    minify_html::minify_file(
-        "app.html",
-        std::env::var("OUT_DIR").expect("Should be set") + "/app.min.html",
-    )
-    .expect("Should minify html");
+    #[cfg(windows)]
+    const NPM: &str = "npm.cmd";
+    #[cfg(not(windows))]
+    const NPM: &str = "npm";
+
+    // Install npm packages if needed
+    if !std::path::Path::new("web/node_modules").exists() {
+        std::process::Command::new(NPM)
+            .arg("install")
+            .current_dir("web")
+            .output()
+            .expect("Failed to run npm install");
+    }
+
+    // Invalidate build when web assets change
+    fn print_rerun(dir: &std::path::Path) {
+        for entry in std::fs::read_dir(dir).expect("Failed to read dir") {
+            let path = entry.expect("Failed to read entry").path();
+            if path.is_dir() {
+                print_rerun(&path);
+            } else {
+                println!("cargo:rerun-if-changed={}", path.display());
+            }
+        }
+    }
+    println!("cargo:rerun-if-changed=web/index.html");
+    print_rerun(std::path::Path::new("web/src"));
+
+    // Build frontend
+    std::process::Command::new(NPM)
+        .arg("run")
+        .arg("build")
+        .current_dir("web")
+        .output()
+        .expect("Failed to run npm run build");
+
+    // Copy built assets to OUT_DIR/web
+    let out_dir = std::env::var("OUT_DIR").expect("$OUT_DIR not set");
+    let dest_path = std::path::Path::new(&out_dir).join("web");
+    if dest_path.exists() {
+        std::fs::remove_dir_all(&dest_path).expect("Failed to remove old web dir");
+    }
+    fn copy_dir_all(src: &std::path::Path, dst: &std::path::Path) -> std::io::Result<()> {
+        std::fs::create_dir_all(dst)?;
+        for entry in std::fs::read_dir(src)? {
+            let entry = entry?;
+            let ty = entry.file_type()?;
+            let dst_path = dst.join(entry.file_name());
+            if ty.is_dir() {
+                copy_dir_all(&entry.path(), &dst_path)?;
+            } else {
+                std::fs::copy(entry.path(), dst_path)?;
+            }
+        }
+        Ok(())
+    }
+    copy_dir_all(std::path::Path::new("web/dist"), &dest_path)
+        .expect("Failed to copy web/dist files to $OUT_DIR");
 }
---
+++ bin/bassielight/web/src/index.jsx
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2025 Leonard van der Plaat
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import { render } from 'preact';
+import './index.css';
+import { App } from './app.jsx';
+
+render(<App />, document.getElementById('app'));
