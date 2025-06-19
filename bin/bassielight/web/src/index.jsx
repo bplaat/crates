a8ce539f2d39e535686f73cdc0aa8048e65dfc13
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-06-19 12:30:11 +0200

    bassielight+example: Disable right mouse menu in webview

--- bin/bassielight/web/src/index.jsx
+++ bin/bassielight/web/src/index.jsx
@@ -8,4 +8,6 @@ import { render } from 'preact';
 import './index.css';
 import { App } from './app.jsx';
 
+window.addEventListener('contextmenu', (event) => event.preventDefault());
+
 render(<App />, document.getElementById('app'));
