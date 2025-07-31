ede132ef97dde4995225edfa0695300496ec7979
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-07-31 11:48:33 +0200

    Add more bond types and wall creation form

--- src/lib.rs
+++ src/lib.rs
@@ -4,43 +4,61 @@
  * SPDX-License-Identifier: MIT
  */
 
+use std::str::FromStr;
+
 use wasm_bindgen::prelude::*;
-use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
+use web_sys::{CanvasRenderingContext2d, Event, HtmlCanvasElement, HtmlFormElement};
+
+// MARK: Constants
+const BRICK_BED_JOINT: f64 = 10.0; // Padding between bricks
+const BRICK_HEAD_JOINT: f64 = 12.5; // Padding between rows
+const BRICK_HALF_WIDTH: f64 = 100.0;
+const BRICK_WIDTH: f64 = BRICK_HALF_WIDTH * 2.0 + BRICK_BED_JOINT;
+const BRICK_TWO_THIRDS_WIDTH: f64 = BRICK_HALF_WIDTH * 1.5 + BRICK_BED_JOINT / 2.0;
+const BRICK_HEIGHT: f64 = 50.0;
+
+const STRIDE_WIDTH: f64 = 800.0; // Robot's horizontal reach
+const STRIDE_HEIGHT: f64 = 1300.0; // Robot's vertical reach
 
 // MARK: Brick
-const BRICK_WIDTH: f64 = 210.0;
-const BRICK_HALF_WIDTH: f64 = 100.0;
-const BRICK_HEIGHT: f64 = 50.0;
-const BRICK_HEAD_JOINT: f64 = 10.0; // Padding between bricks
-const BRICK_BED_JOINT: f64 = 12.5; // Padding between rows
-
-enum BrickType {
-    Normal,
-    Half,
-}
-
 struct Brick {
-    r#type: BrickType,
     x: f64,
     y: f64,
+    width: f64,
 }
 
 impl Brick {
-    fn new(r#type: BrickType, x: f64, y: f64) -> Self {
-        Brick { r#type, x, y }
+    fn new(x: f64, y: f64, width: f64) -> Self {
+        Brick { x, y, width }
     }
 
     fn draw(&self, context: &CanvasRenderingContext2d) {
         context.set_fill_style_str("red");
-        let width = match self.r#type {
-            BrickType::Normal => BRICK_WIDTH,
-            BrickType::Half => BRICK_HALF_WIDTH,
-        };
-        context.fill_rect(self.x, self.y, width, BRICK_HEIGHT);
+        context.fill_rect(self.x, self.y, self.width, BRICK_HEIGHT);
     }
 }
 
 // MARK: Wall
+enum BondType {
+    Stretcher, // Normal bricks
+    Header,    // Half bricks
+    English,   // Half bricks every alternating row
+    Flemish,   // Half bricks alternating
+}
+
+impl FromStr for BondType {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "stretcher" => Ok(BondType::Stretcher),
+            "header" => Ok(BondType::Header),
+            "english" => Ok(BondType::English),
+            "flemish" => Ok(BondType::Flemish),
+            _ => Err(()),
+        }
+    }
+}
+
 struct Wall {
     width: f64,
     height: f64,
@@ -48,43 +66,137 @@ struct Wall {
 }
 
 impl Wall {
-    fn new(width: f64, height: f64) -> Self {
-        let cols = (width / (BRICK_WIDTH + BRICK_HEAD_JOINT)).ceil() as usize;
-        let rows = (height / (BRICK_HEIGHT + BRICK_BED_JOINT)).ceil() as usize;
-
+    fn new(width: f64, height: f64, bond: BondType) -> Self {
+        // FIXME: Make brick filling algo more flexible
+        let rows = (height / (BRICK_HEIGHT + BRICK_HEAD_JOINT)).ceil() as usize;
         let mut bricks = Vec::new();
+        let mut y = height - BRICK_HEIGHT;
         for j in 0..rows {
-            let y = j as f64 * (BRICK_HEIGHT + BRICK_BED_JOINT) + BRICK_BED_JOINT;
-            // On odd rows, start and end with a half brick
-            if j % 2 == 0 {
-                let mut x = 0.0;
+            match bond {
+                // MARK: Stretcher bond
+                BondType::Stretcher => {
+                    let cols = (width / (BRICK_WIDTH + BRICK_BED_JOINT)).ceil() as usize;
+                    let mut x = 0.0;
+                    if j % 2 == 0 {
+                        // Normal bricks
+                        for _ in 0..(cols - 1) {
+                            bricks.push(Brick::new(x, y, BRICK_WIDTH));
+                            x += BRICK_WIDTH + BRICK_BED_JOINT;
+                        }
 
-                // Normal bricks
-                for _ in 0..(cols - 1) {
-                    let brick = Brick::new(BrickType::Normal, x, y);
-                    bricks.push(brick);
-                    x += BRICK_WIDTH + BRICK_HEAD_JOINT;
+                        // End half brick
+                        bricks.push(Brick::new(x, y, BRICK_HALF_WIDTH));
+                    } else {
+                        // Start half brick
+                        bricks.push(Brick::new(x, y, BRICK_HALF_WIDTH));
+                        x += BRICK_HALF_WIDTH + BRICK_BED_JOINT;
+
+                        // Normal bricks
+                        for _ in 0..(cols - 1) {
+                            bricks.push(Brick::new(x, y, BRICK_WIDTH));
+                            x += BRICK_WIDTH + BRICK_BED_JOINT;
+                        }
+                    }
                 }
 
-                // End half brick
-                let end_brick = Brick::new(BrickType::Half, x, y);
-                bricks.push(end_brick);
-            } else {
-                let mut x = 0.0;
+                // MARK: Header bond
+                BondType::Header => {
+                    let cols = (width / (BRICK_HALF_WIDTH + BRICK_BED_JOINT)).ceil() as usize;
+                    let mut x = 0.0;
+                    if j % 2 == 0 {
+                        // Half bricks
+                        for _ in 0..cols {
+                            bricks.push(Brick::new(x, y, BRICK_HALF_WIDTH));
+                            x += BRICK_HALF_WIDTH + BRICK_BED_JOINT;
+                        }
+                    } else {
+                        // Start two thirds brick
+                        bricks.push(Brick::new(x, y, BRICK_TWO_THIRDS_WIDTH));
 
-                // Start half brick
-                let start_brick = Brick::new(BrickType::Half, x, y);
-                bricks.push(start_brick);
+                        x += BRICK_TWO_THIRDS_WIDTH + BRICK_BED_JOINT;
 
-                x += BRICK_HALF_WIDTH + BRICK_HEAD_JOINT;
+                        // Half bricks
+                        for _ in 0..(cols - 3) {
+                            bricks.push(Brick::new(x, y, BRICK_HALF_WIDTH));
+                            x += BRICK_HALF_WIDTH + BRICK_BED_JOINT;
+                        }
 
-                // Normal bricks
-                for _ in 0..(cols - 1) {
-                    let brick = Brick::new(BrickType::Normal, x, y);
-                    bricks.push(brick);
-                    x += BRICK_WIDTH + BRICK_HEAD_JOINT;
+                        // End two thirds brick
+                        bricks.push(Brick::new(x, y, BRICK_TWO_THIRDS_WIDTH));
+                    }
+                }
+
+                // MARK: English bond
+                BondType::English => {
+                    let cols = ((width - BRICK_TWO_THIRDS_WIDTH) / (BRICK_WIDTH + BRICK_BED_JOINT))
+                        .ceil() as usize;
+                    let mut x = 0.0;
+                    if j % 2 == 0 {
+                        // Normal bricks
+                        for _ in 0..cols {
+                            bricks.push(Brick::new(x, y, BRICK_WIDTH));
+                            x += BRICK_WIDTH + BRICK_BED_JOINT;
+                        }
+
+                        // Half brick at the end
+                        bricks.push(Brick::new(x, y, BRICK_HALF_WIDTH));
+                    } else {
+                        // Start two thirds brick
+                        bricks.push(Brick::new(x, y, BRICK_TWO_THIRDS_WIDTH));
+
+                        x += BRICK_TWO_THIRDS_WIDTH + BRICK_BED_JOINT;
+
+                        // Half bricks
+                        for _ in 0..((cols - 1) * 2) {
+                            bricks.push(Brick::new(x, y, BRICK_HALF_WIDTH));
+                            x += BRICK_HALF_WIDTH + BRICK_BED_JOINT;
+                        }
+
+                        // End two thirds brick
+                        bricks.push(Brick::new(x, y, BRICK_TWO_THIRDS_WIDTH));
+                    }
+                }
+
+                // MARK: Flemish bond
+                BondType::Flemish => {
+                    let cols = ((width - BRICK_TWO_THIRDS_WIDTH) / (BRICK_WIDTH + BRICK_BED_JOINT))
+                        .ceil() as usize;
+                    let mut x = 0.0;
+                    let len = cols * 3 / 2 - 1;
+                    if j % 2 == 0 {
+                        // Normal or half brick
+                        for i in 0..len {
+                            let width = if i % 2 == 0 {
+                                BRICK_WIDTH
+                            } else {
+                                BRICK_HALF_WIDTH
+                            };
+                            bricks.push(Brick::new(x, y, width));
+                            x += width + BRICK_BED_JOINT;
+                        }
+                    } else {
+                        // Start two thirds brick
+                        bricks.push(Brick::new(x, y, BRICK_TWO_THIRDS_WIDTH));
+                        x += BRICK_TWO_THIRDS_WIDTH + BRICK_BED_JOINT;
+
+                        // Normal or half brick
+                        for i in 0..len - 2 {
+                            let width = if i % 2 == 0 {
+                                BRICK_WIDTH
+                            } else {
+                                BRICK_HALF_WIDTH
+                            };
+                            bricks.push(Brick::new(x, y, width));
+                            x += width + BRICK_BED_JOINT;
+                        }
+
+                        // End two thirds brick
+                        bricks.push(Brick::new(x, y, BRICK_TWO_THIRDS_WIDTH));
+                    }
                 }
             }
+
+            y -= BRICK_HEIGHT + BRICK_HEAD_JOINT;
         }
         Wall {
             width,
@@ -102,33 +214,105 @@ impl Wall {
         context.set_stroke_style_str("green");
         context.set_line_width(5.0);
         context.stroke_rect(0.0, 0.0, self.width, self.height);
+
+        // Draw robot's reach
+        context.set_stroke_style_str("blue");
+        context.stroke_rect(
+            0.0,
+            self.height - STRIDE_HEIGHT,
+            STRIDE_WIDTH,
+            STRIDE_HEIGHT,
+        );
     }
 }
 
 // MARK: Main
 #[wasm_bindgen]
 pub fn main() -> Result<(), JsValue> {
-    // Get canvas context
-    let window = web_sys::window().expect("Should have window");
-    let document = window.document().expect("Should have document");
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
 
+    // Get canvas context
     let canvas = document
         .get_element_by_id("canvas")
-        .expect("Should find canvas element")
+        .unwrap()
         .dyn_into::<HtmlCanvasElement>()?;
     let context = canvas
         .get_context("2d")?
-        .expect("Should have 2d context")
+        .unwrap()
         .dyn_into::<CanvasRenderingContext2d>()?;
 
-    // FIXME: Add scaling controls
-    context.scale(0.35, 0.35)?;
+    // Wall form
+    let wall_form = document
+        .get_element_by_id("wall-form")
+        .unwrap()
+        .dyn_into::<HtmlFormElement>()?;
+    let wall_width_input = document
+        .get_element_by_id("wall-width")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlInputElement>()?;
+    let wall_height_input = document
+        .get_element_by_id("wall-height")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlInputElement>()?;
+    let wall_bond_select = document
+        .get_element_by_id("wall-bond")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlSelectElement>()?;
+    {
+        let wall_width_input = wall_width_input.clone();
+        let wall_height_input = wall_height_input.clone();
+        let wall_bond_select = wall_bond_select.clone();
+        let canvas = canvas.clone();
+        let context = context.clone();
+
+        let closure = Closure::wrap(Box::new(move |event: Event| {
+            event.prevent_default();
+
+            // Create wall and draw
+            let wall = Wall::new(
+                wall_width_input.value().parse().unwrap(),
+                wall_height_input.value().parse().unwrap(),
+                wall_bond_select.value().parse::<BondType>().unwrap(),
+            );
+
+            // Scale the canvas to fit the wall
+            let scale = canvas.width() as f64 / (wall.width * 1.2);
+            context.reset();
+            context.scale(scale, scale).unwrap();
+
+            // Draw wall
+            context.clear_rect(
+                0.0,
+                0.0,
+                canvas.width() as f64 * scale,
+                canvas.height() as f64 * scale,
+            );
+            wall.draw(&context);
+        }) as Box<dyn FnMut(_)>);
+        wall_form.add_event_listener_with_callback("submit", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
 
     // Create wall and draw
-    const ASSIGNMENT_WALL_WIDTH: f64 = 2300.0;
-    const ASSIGNMENT_WALL_HEIGHT: f64 = 2000.0;
-    let wall = Wall::new(ASSIGNMENT_WALL_WIDTH, ASSIGNMENT_WALL_HEIGHT);
-    context.clear_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
+    let wall = Wall::new(
+        wall_width_input.value().parse().unwrap(),
+        wall_height_input.value().parse().unwrap(),
+        wall_bond_select.value().parse::<BondType>().unwrap(),
+    );
+
+    // Scale the canvas to fit the wall
+    let scale = canvas.width() as f64 / (wall.width * 1.2);
+    context.reset();
+    context.scale(scale, scale).unwrap();
+
+    // Draw wall
+    context.clear_rect(
+        0.0,
+        0.0,
+        canvas.width() as f64 * scale,
+        canvas.height() as f64 * scale,
+    );
     wall.draw(&context);
 
     Ok(())
