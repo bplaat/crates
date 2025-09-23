c5e55e7639b322c467f033237769427ac44d49ad
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-09-23 15:02:49 +0200

    todo-app: Clean up styles and fix bug with completed checkbox

--- examples/todo-app/web/src/app.jsx
+++ examples/todo-app/web/src/app.jsx
@@ -27,21 +27,21 @@ export function App() {
         setTodos([...todos, { id: crypto.randomUUID(), text: input, completed: false }]);
         setInput('');
     };
-    const completeTodo = (index) => {
-        setTodos(todos.map((todo, i) => (i === index ? { ...todo, completed: !todo.completed } : todo)));
-    };
     const removeCompleted = (e) => {
         e.preventDefault();
         setTodos(todos.filter((todo) => !todo.completed));
     };
+    const completeTodo = (index, completed) => {
+        setTodos(todos.map((todo, i) => (i === index ? { ...todo, completed } : todo)));
+    };
 
     return (
-        <div>
+        <>
             <h1>Todo App</h1>
             <form onSubmit={addTodo}>
                 <input type="text" value={input} onInput={(e) => setInput(e.target.value)} placeholder="Add a todo" />
                 <button type="submit">Add</button>
-                <button onClick={removeCompleted} disabled={todos.length === 0}>
+                <button type="button" onClick={removeCompleted} disabled={todos.length === 0}>
                     Clear done
                 </button>
             </form>
@@ -53,13 +53,21 @@ export function App() {
             ) : (
                 <ul>
                     {todos.map((todo, i) => (
-                        <li key={i} class={todo.completed ? 'is-completed' : ''} onClick={() => completeTodo(i)}>
-                            <input type="checkbox" checked={todo.completed} onChange={() => completeTodo(i)} />
+                        <li
+                            key={i}
+                            class={todo.completed ? 'is-completed' : ''}
+                            onClick={() => completeTodo(i, !todo.completed)}
+                        >
+                            <input
+                                type="checkbox"
+                                checked={todo.completed}
+                                onChange={(e) => completeTodo(i, e.target.checked)}
+                            />
                             {todo.text}
                         </li>
                     ))}
                 </ul>
             )}
-        </div>
+        </>
     );
 }
