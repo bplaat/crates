2a1037764190ef066e0cbfc294d1138831a97866
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-09-28 13:26:06 +0200

    game2048: Add missing Windows resources build.rs step

---
+++ bin/game2048/build.rs
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 Bastiaan van der Plaat
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+//! A local version of the 2048 game
+
+fn main() {
+    // Compile Windows resources
+    #[cfg(windows)]
+    {
+        let manifest = format!(
+            r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <assemblyIdentity type="win32" name="2048" version="{}.0" processorArchitecture="*"/>
+    <description>2048</description>
+
+    <dependency>
+        <dependentAssembly>
+            <assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls" version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*"/>
+        </dependentAssembly>
+    </dependency>
+
+    <compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1">
+        <application>
+            <!-- Windows 10 and Windows 11 -->
+            <supportedOS Id="{{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}}"/>
+        </application>
+    </compatibility>
+
+    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+        <security>
+            <requestedPrivileges>
+                <requestedExecutionLevel level="asInvoker" uiAccess="false"/>
+            </requestedPrivileges>
+        </security>
+    </trustInfo>
+</assembly>
+"#,
+            env!("CARGO_PKG_VERSION")
+        );
+
+        let mut res = winres::WindowsResource::new();
+        res.set("ProductName", "2048")
+            .set("FileDescription", "2048")
+            .set(
+                "LegalCopyright",
+                "Copyright © 2014 Gabriele Cirulli\nCopyright © 2025 Bastiaan van der Plaat",
+            )
+            .set_icon("meta/windows/icon.ico")
+            .set_manifest(&manifest);
+        res.compile().expect("Failed to compile Windows resources.");
+    }
+}
