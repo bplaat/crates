d8c115369ff29a91fb4fee642f53774af0b16714
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-09-30 11:22:10 +0200

    game2048: Use localStorage instead of cookies

--- bin/game2048/web/js/local_storage_manager.js
+++ bin/game2048/web/js/local_storage_manager.js
@@ -8,42 +8,28 @@
 function LocalStorageManager() {
     this.bestScoreKey = 'bestScore';
     this.gameStateKey = 'gameState';
+    this.storage = window.localStorage;
 }
-
-// Helper functions for cookies
-LocalStorageManager.prototype._getCookie = function (name) {
-    const value = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
-    return value ? decodeURIComponent(value.pop()) : null;
-};
-
-LocalStorageManager.prototype._setCookie = function (name, value, days = 365) {
-    const expires = new Date(Date.now() + days * 864e5).toUTCString();
-    document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=/';
-};
-
-LocalStorageManager.prototype._removeCookie = function (name) {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
-};
 
 // Best score getters/setters
 LocalStorageManager.prototype.getBestScore = function () {
-    return this._getCookie(this.bestScoreKey) || 0;
+    return this.storage.getItem(this.bestScoreKey) || 0;
 };
 
 LocalStorageManager.prototype.setBestScore = function (score) {
-    this._setCookie(this.bestScoreKey, score);
+    this.storage.setItem(this.bestScoreKey, score);
 };
 
 // Game state getters/setters and clearing
 LocalStorageManager.prototype.getGameState = function () {
-    var stateJSON = this._getCookie(this.gameStateKey);
+    var stateJSON = this.storage.getItem(this.gameStateKey);
     return stateJSON ? JSON.parse(stateJSON) : null;
 };
 
 LocalStorageManager.prototype.setGameState = function (gameState) {
-    this._setCookie(this.gameStateKey, JSON.stringify(gameState));
+    this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
 };
 
 LocalStorageManager.prototype.clearGameState = function () {
-    this._removeCookie(this.gameStateKey);
+    this.storage.removeItem(this.gameStateKey);
 };
