02746e1faabd7b964f76a040813d6bb09a90b0ca
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-12-16 16:45:22 +0100

    bwebview: Fix building on Windows with GNU toolchain

--- lib/bwebview/build.rs
+++ lib/bwebview/build.rs
@@ -10,26 +10,44 @@ fn main() {
     #[cfg(windows)]
     {
         // Link with the correct WebView2Loader library based on architecture
+        let target = std::env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH not set");
         let lib_dir = std::path::PathBuf::from(
             std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set"),
         )
         .join("webview2")
-        .join(if cfg!(target_arch = "x86_64") {
+        .join(if target == "x86_64" {
             "x64"
-        } else if cfg!(target_arch = "aarch64") {
+        } else if target == "aarch64" {
             "arm64"
-        } else if cfg!(target_arch = "x86") {
+        } else if target == "x86" {
             "x86"
         } else {
             panic!("Unsupported architecture")
         });
 
         println!("cargo:rustc-link-search=native={}", lib_dir.display());
-        if cfg!(target_env = "msvc") {
+        let target_env =
+            std::env::var("CARGO_CFG_TARGET_ENV").expect("CARGO_CFG_TARGET_ENV not set");
+        if target_env == "msvc" {
             println!("cargo:rustc-link-lib=static=WebView2LoaderStatic");
         } else {
             println!("cargo:rustc-link-lib=dylib=WebView2Loader");
-            // FIXME: Maybe copy WebView2Loader.dll to output dir?
+
+            // Copy WebView2Loader.dll to output directory for dynamic linking
+            let out_dir =
+                std::path::PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR not set"));
+            std::fs::copy(
+                lib_dir.join("WebView2Loader.dll"),
+                out_dir
+                    .parent()
+                    .expect("Should be some")
+                    .parent()
+                    .expect("Should be some")
+                    .parent()
+                    .expect("Should be some")
+                    .join("WebView2Loader.dll"),
+            )
+            .expect("Failed to copy WebView2Loader.dll");
         }
     }
 }
