67bc58319fcc389e05e00fe003fc839458e857d6
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2026-06-03 19:06:16 +0200

    example-todo-app: Add support for older browser versions

--- examples/todo-app/web/index.js
+++ examples/todo-app/web/index.js
@@ -4,6 +4,15 @@
  * SPDX-License-Identifier: MIT
  */
 
+// Polyfills
+if (!crypto.randomUUID) {
+    crypto.randomUUID = () =>
+        'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            const r = (Math.random() * 16) | 0;
+            return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+        });
+}
+
 window.addEventListener('contextmenu', (event) => event.preventDefault());
 
 function ipcSend(type, data = {}) {
