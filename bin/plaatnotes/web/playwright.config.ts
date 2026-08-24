7dfa48b5051795401a1462bcaf21541dde51a323
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2026-08-24 18:44:16 +0200

    plaatnotes: Don't retry e2e test locally

--- bin/plaatnotes/web/playwright.config.ts
+++ bin/plaatnotes/web/playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     testDir: './e2e',
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
-    retries: 2,
+    retries: process.env.CI ? 1 : 0,
     workers: process.env.CI ? 1 : undefined,
     reporter: process.env.CI ? [['list'], ['junit', { outputFile: 'test-results/junit.xml' }]] : 'list',
     expect: {
