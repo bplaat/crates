3db9081c8eb971d12507573db9046c988d761fe0
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2026-08-27 09:04:49 +0200

    binman: Guard build.rs to only Windows

--- bin/binman/build.rs
+++ bin/binman/build.rs
@@ -7,6 +7,11 @@
 #![doc = include_str!("README.md")]
 
 fn main() {
+    // Compile Windows resources
+    if std::env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set") != "windows" {
+        return;
+    }
+
     let manifest = format!(
         r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" xmlns:asmv3="urn:schemas-microsoft-com:asm.v3" manifestVersion="1.0">
